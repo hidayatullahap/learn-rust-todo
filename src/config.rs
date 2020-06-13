ddd0328e649b2e93caab3fe8d9961357fc8bf67c
Hidayatullah Agung <hidayatullah@qasir.id>
2020-06-14 01:30:04 +0700

    add log

---
+++ src/config.rs
@@ -0,0 +1,34 @@
+use config::ConfigError;
+use serde::Deserialize;
+use slog::{o, Drain, Logger};
+use slog_async;
+use slog_envlogger;
+use slog_term;
+
+#[derive(Deserialize)]
+pub struct ServerConfig {
+    pub host: String,
+    pub port: i32,
+}
+
+#[derive(Deserialize)]
+pub struct Config {
+    pub server: ServerConfig,
+}
+
+impl Config {
+    pub fn from_env() -> Result<Self, ConfigError> {
+        let mut conf = config::Config::new();
+
+        conf.merge(config::Environment::new())?;
+        conf.try_into()
+    }
+
+    pub fn configure_log() -> Logger {
+        let decorator = slog_term::TermDecorator::new().build();
+        let console_drain = slog_term::FullFormat::new(decorator).build().fuse();
+        let console_drain = slog_envlogger::new(console_drain);
+        let console_drain = slog_async::Async::new(console_drain).build().fuse();
+        slog::Logger::root(console_drain, o!("v" => env!("CARGO_PKG_VERSION")))
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,26 @@
+mod config;
 mod models;
 
+use crate::config::Config;
 use crate::models::Status;
 use actix_web::{web, App, HttpServer, Responder};
+use dotenv::dotenv;
+use slog::info;
 use std::io;
 
 #[actix_rt::main]
 async fn main() -> io::Result<()> {
+    dotenv().ok();
+    let config = Config::from_env().unwrap();
+    let log = Config::configure_log();
+
+    info!(
+        log,
+        "Starting server at http://{}:{}", config.server.host, config.server.port
+    );
+
     HttpServer::new(|| App::new().route("/", web::get().to(status)))
-        .bind("127.0.0.1:8000")?
+        .bind(format!("{}:{}", config.server.host, config.server.port))?
         .run()
         .await
 }
